07d0a89ddc42d8d16408864204c80b5cadc2ae9e
Vlad <vlad.promotorov@studiomobile.ru>
2019-01-29 19:08:03 +0300

    Add OrdersListRecyclerView on the MainActivity

---
+++ Android/src/MainActivity/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+
+namespace Android
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        RecyclerView OffersListRecyclerView;
+        RecyclerView.Adapter OffersListAdapter;
+        RecyclerView.LayoutManager OffersListLayoutManager;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            OffersListRecyclerView = (RecyclerView)FindViewById(Resource.Id.offers_recycler_view);
+
+            OffersListRecyclerView.HasFixedSize = true;
+
+            OffersListLayoutManager = new LinearLayoutManager(this);
+            OffersListRecyclerView.SetLayoutManager(OffersListLayoutManager);
+            
+            OffersListAdapter = new OffersListAdapter(new string[] {"first", "second", "third", "forth"});
+            OffersListRecyclerView.SetAdapter(OffersListAdapter);
+        }
+    }
+}
---
+++ Android/src/MainActivity/OffersListAdapter.cs
@@ -0,0 +1,39 @@
+﻿using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+
+namespace Android
+{
+    public class OffersListAdapter : RecyclerView.Adapter
+    {
+        private string[] testData;
+
+        public override int ItemCount => testData.Length;
+
+        public class OfferViewHolder : RecyclerView.ViewHolder
+        {
+            public TextView IdTextView;
+            public OfferViewHolder(TextView textView) : base(textView)
+            {
+                IdTextView = textView;
+            }
+        }
+
+        public OffersListAdapter(string[] data)
+        {
+            testData = data;
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            return new OfferViewHolder(new TextView(parent.Context)); 
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+
+            OfferViewHolder offerViewHolder = holder as OfferViewHolder;
+            offerViewHolder.IdTextView.Text = testData[position];
+        }
+    }
+}
