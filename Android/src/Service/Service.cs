7bd9b72f6012a171a0a33fa83872e4781918e4ec
Vlad <vlad.promotorov@studiomobile.ru>
2019-01-31 16:23:11 +0300

    Load and parse XMLDocument assynchronously

--- Android/src/Service/Service.cs
+++ Android/src/Service/Service.cs
@@ -20,35 +20,35 @@ namespace Android
 
         public async Task<List<Offer>> GetOffers()
         {
-            await Task.Delay(2000);
+            return await Task.Run(() => {
+                XmlDocument document = new XmlDocument();
+                document.Load(_baseUri.ToString());
 
-            XmlDocument document = new XmlDocument();
-            document.Load(_baseUri.ToString());
+                XmlNode node = document.DocumentElement.SelectSingleNode("/yml_catalog/shop/offers");
 
-            XmlNode node = document.DocumentElement.SelectSingleNode("/yml_catalog/shop/offers");
-
-            List<Offer> offers = new List<Offer>();
-            foreach (XmlNode xnode in node.ChildNodes)
-            {
-                Offer offer = new Offer();
-                offer.Id = xnode.Attributes["id"]?.Value;
-                offer.Bid = xnode.Attributes["bid"]?.Value;
-                offer.Type = xnode.Attributes["type"]?.Value;
-                offer.IsAvailable = xnode.Attributes["available"]?.Value;
-                offer.Url = xnode["url"]?.InnerText;
-                offer.Price = xnode["price"]?.InnerText;
-                offer.CurrencyId = xnode["currencyId"]?.InnerText;
-                offer.CategoryId = xnode["categoryId"]?.InnerText;
-                offer.PictureUrl = xnode["picture"]?.InnerText;
-                offer.Delivery = xnode["delivery"]?.InnerText;
-                offer.LocalDeliveryCost = xnode["local_delivery_cost"]?.InnerText;
-                offer.TypePrefix = xnode["typePrefix"]?.InnerText;
-                offer.Vendor = xnode["vendor"]?.InnerText;  
-                offer.VendorCode = xnode["vendorCode"]?.InnerText;
-                offer.Model = xnode["model"]?.InnerText;
-                offers.Add(offer);
-            }
-            return offers;
+                List<Offer> offers = new List<Offer>();
+                foreach (XmlNode xnode in node.ChildNodes)
+                {
+                    Offer offer = new Offer();
+                    offer.Id = xnode.Attributes["id"]?.Value;
+                    offer.Bid = xnode.Attributes["bid"]?.Value;
+                    offer.Type = xnode.Attributes["type"]?.Value;
+                    offer.IsAvailable = xnode.Attributes["available"]?.Value;
+                    offer.Url = xnode["url"]?.InnerText;
+                    offer.Price = xnode["price"]?.InnerText;
+                    offer.CurrencyId = xnode["currencyId"]?.InnerText;
+                    offer.CategoryId = xnode["categoryId"]?.InnerText;
+                    offer.PictureUrl = xnode["picture"]?.InnerText;
+                    offer.Delivery = xnode["delivery"]?.InnerText;
+                    offer.LocalDeliveryCost = xnode["local_delivery_cost"]?.InnerText;
+                    offer.TypePrefix = xnode["typePrefix"]?.InnerText;
+                    offer.Vendor = xnode["vendor"]?.InnerText;
+                    offer.VendorCode = xnode["vendorCode"]?.InnerText;
+                    offer.Model = xnode["model"]?.InnerText;
+                    offers.Add(offer);
+                }
+                return offers;
+            });
         }
     }
 }
